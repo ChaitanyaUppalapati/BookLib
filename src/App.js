58fb5f1c3c50945e82c3b3ee1f107104174c19b0
ChaitanyaUppalapati <77841750+ChaitanyaUppalapati@users.noreply.github.com>
2023-07-07 17:31:55 +0530

    login update

--- src/App.js
+++ src/App.js
@@ -19,24 +19,7 @@ import { useState } from 'react';
 const queryClient = new QueryClient()
 
 function App() {
-<<<<<<< HEAD
-=======
-  const { user, isAuthenticated } = useAuth();
-  const loginWithRedirect = useLoginWithRedirect();
 
-  useEffect(() => {
-    if (!isAuthenticated) {
-  loginWithRedirect();
-    }
-  }, [isAuthenticated, loginWithRedirect]);
-
-
-  function logout() {
-    const baseUrl = ContextHolder.getContext().baseUrl;
-    window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location.origin}`;
-  };
-
->>>>>>> e1717708285d0b7bf64f4d7012bf566f894e5ee6
   
   const authenticate=getAuth();
   const user=authenticate.currentUser;
@@ -46,11 +29,11 @@ function App() {
   <QueryClientProvider client={queryClient}>
       <NavBar signedUser={signedUser} setUser={setUser} />
     <Routes>
-      <Route path="/" element={<Library signedUser={signedUser} />} />
-      <Route path="/cart" element={<Cart signedUser={signedUser} />} />
-      <Route path="/login" element={<Login signedUser={signedUser} setUser={setUser}/>} />
-      <Route path="/register" element={<Register signedUser={signedUser} setUser={setUser} />} />
-      <Route path="/book/:name" element={<Book signedUser={signedUser} />} />
+      <Route path="/BookLib" element={<Library signedUser={signedUser} />} />
+      <Route path="/BookLib/cart" element={<Cart signedUser={signedUser} />} />
+      <Route path="/BookLib/login" element={<Login signedUser={signedUser} setUser={setUser}/>} />
+      <Route path="/BookLib/register" element={<Register signedUser={signedUser} setUser={setUser} />} />
+      <Route path="/BookLib/book/:name" element={<Book signedUser={signedUser} />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </QueryClientProvider>
--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,5 +1,10 @@
+import {Link} from 'react-router-dom';
+import {Button} from '@mui/material';
 export default function NotFound(){
     return <>
-        Not Found....
+        <div>
+            <h3>PAGE NOT FOUND...</h3>
+            <Button variant="outlined"><Link to='/BookLib' />HOME</Button>
+        </div>
     </>
 }
